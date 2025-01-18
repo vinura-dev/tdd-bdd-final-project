9eb230120ee0fce8817c0c8acdf7d59940e60d2c
vinura-dev <vinura.devinuwara@gmail.com>
2025-01-17 20:27:55 -0500

    add rest of the app routes; create tests for update and delete

--- service/routes.py
+++ service/routes.py
@@ -20,7 +20,7 @@ Product Store Service with UI
 """
 from flask import jsonify, request, abort
 from flask import url_for  # noqa: F401 pylint: disable=unused-import
-from service.models import Product
+from service.models import Product, Category
 from service.common import status  # HTTP Status Codes
 from . import app
 
@@ -125,15 +125,79 @@ def get_products(product_id):
 # U P D A T E   A   P R O D U C T
 ######################################################################
 
-#
-# PLACE YOUR CODE TO UPDATE A PRODUCT HERE
-#
+
+@app.route("/products/<int:product_id>", methods=["PUT"])
+def update_products(product_id):
+    """
+    Update a Product
+    This endpoint will update a Product based on the body that is posted
+    """
+    app.logger.info("Request to Update a product with id [%s]", product_id)
+    check_content_type("application/json")
+
+    product = Product.find(product_id)
+    if not product:
+        abort(status.HTTP_404_NOT_FOUND, f"Product with id '{product_id}' was not found.")
+    # Update information
+    product.deserialize(request.get_json())
+    product.update()
+    message = product.serialize()
+
+    return jsonify(message), status.HTTP_200_OK
 
 ######################################################################
 # D E L E T E   A   P R O D U C T
 ######################################################################
 
 
-#
-# PLACE YOUR CODE TO DELETE A PRODUCT HERE
-#
+@app.route("/products/<int:product_id>", methods=["DELETE"])
+def delete_products(product_id):
+    """
+    Delete a Product
+    This endpoint will delete a Product based the id specified in the path
+    """
+    app.logger.info("Request to Delete a product with id [%s]", product_id)
+
+    product = Product.find(product_id)
+    if not product:
+        abort(status.HTTP_404_NOT_FOUND, f"Product with id '{product_id}' was not found.")
+
+    product.delete()
+    message = ""
+    return jsonify(message), status.HTTP_204_NO_CONTENT
+
+######################################################################
+# L I S T  A L L  P R O D U C T S
+######################################################################
+
+
+@app.route("/products", methods=["GET"])
+def list_all_products():
+    """
+    List all Products
+    This endpoint will return a list of Products
+    """
+    app.logger.info("Request to list Products...")
+
+    products = []
+    name = request.args.get("name")
+    category = request.args.get("category")
+    available = request.args.get("available")
+
+    if name:
+        app.logger.info("Find by name [%s]", name)
+        products = Product.find_by_name(name)
+    elif category:
+        app.logger.info("Find by category [%s]", category)
+        category_val = getattr(Category, category.upper())
+        products = Product.find_by_category(category_val)
+    elif available:
+        app.logger.info("Find by availability [%s]", available)
+        available_val = available.lower() in ["true", "yes", "1"]
+        products = Product.find_by_availability(available_val)
+    else:
+        app.logger.info("Find all")
+        products = Product.all()
+
+    message = [product.serialize() for product in products]
+    return jsonify(message), status.HTTP_200_OK
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -130,19 +130,15 @@ class TestProductRoutes(TestCase):
         self.assertEqual(new_product["available"], test_product.available)
         self.assertEqual(new_product["category"], test_product.category.name)
 
-        #
-        # Uncomment this code once READ is implemented
-        #
-
-        # # Check that the location header was correct
-        # response = self.client.get(location)
-        # self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # new_product = response.get_json()
-        # self.assertEqual(new_product["name"], test_product.name)
-        # self.assertEqual(new_product["description"], test_product.description)
-        # self.assertEqual(Decimal(new_product["price"]), test_product.price)
-        # self.assertEqual(new_product["available"], test_product.available)
-        # self.assertEqual(new_product["category"], test_product.category.name)
+        # Check that the location header was correct
+        response = self.client.get(location)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        new_product = response.get_json()
+        self.assertEqual(new_product["name"], test_product.name)
+        self.assertEqual(new_product["description"], test_product.description)
+        self.assertEqual(Decimal(new_product["price"]), test_product.price)
+        self.assertEqual(new_product["available"], test_product.available)
+        self.assertEqual(new_product["category"], test_product.category.name)
 
     def test_create_product_with_no_name(self):
         """It should not Create a Product without a name"""
@@ -178,6 +174,34 @@ class TestProductRoutes(TestCase):
         invalid_product_id = 0
         response = self.client.get(f"{BASE_URL}/{invalid_product_id}")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_update_product(self):
+        """It should update an existing product"""
+        # create a product
+        test_product = ProductFactory()
+        response = self.client.post(BASE_URL, json=test_product.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # update the product
+        product_data = response.get_json()
+        product_data["description"] = "unknown"
+        response = self.client.put(f"{BASE_URL}/{product_data['id']}", json=product_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        updated_product = response.get_json()
+        self.assertEqual(updated_product["description"], "unknown")
+    
+    def test_delete_product(self):
+        """It should update an existing product"""
+        # create a product
+        test_product = ProductFactory()
+        response = self.client.post(BASE_URL, json=test_product.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # delete the product
+        product_data = response.get_json()
+        response = self.client.delete(f"{BASE_URL}/{product_data['id']}")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        response = self.client.get(f"{BASE_URL}/{product_data['id']}")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
 
     ######################################################################
     # Utility functions
